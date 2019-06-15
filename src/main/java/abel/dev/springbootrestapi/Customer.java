7dcaeef2780cdaf911812c285589adf7d6b14270
Abel Fresnillo <me@abel.dev>
2019-06-15 16:59:43 -0400

    first commit

---
+++ src/main/java/abel/dev/springbootrestapi/Customer.java
@@ -0,0 +1,75 @@
+package abel.dev.springbootrestapi;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Customer {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private Date birthdate;
+    private Address address;
+
+    public Customer(String firstName, String lastName, Date birthdate, Address address) {
+        this.id = UUID.randomUUID().toString();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthdate = birthdate;
+        this.address = address;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Customer customer = (Customer) o;
+
+        return id.equals(customer.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+}
